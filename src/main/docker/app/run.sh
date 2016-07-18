03a30004648eaf028926eacb4b3b8ca192d86658
suport.cloud <suport.cloud@gencat.cat>
2016-07-18 10:07:01 +0200

    nom del war

--- src/main/docker/app/run.sh
+++ src/main/docker/app/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar /tmp/springbootdemo-0.0.1.war
+java -Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar /tmp/springbootdemo-1.0.0.war
